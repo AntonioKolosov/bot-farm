e06dd780865e888c3ffd772267dae440dd5bc24a
evgeny <ekarpovsky@gmail.com>
2023-04-23 12:16:19 +0000

    Implement in main tunnel auto set/unset for dev

--- main.py
+++ main.py
@@ -2,8 +2,27 @@
 For development purpose only
 """
 
+import os
+
 import uvicorn
+from pyngrok import ngrok
 
 
 if __name__ == "__main__":
-    uvicorn.run("app.app:app", port=8005, reload=True)
+    PORT = 8005
+    # Open a HTTP tunnel on the default port 80
+    # <NgrokTunnel: "http://<public_sub>.ngrok.io" ->
+    # "http://localhost:PORT">
+    http_tunnel = ngrok.connect(PORT, bind_tls=True)
+    public_url = http_tunnel.public_url
+    os.environ['API_URL'] = public_url
+    print("public URL", public_url)
+
+    # Run the server
+    uvicorn.run("app.app:app", port=PORT, host="127.0.0.1",
+                log_level="info", reload=True)
+
+    # Close the HTTP tunnel
+    print("disconnect and kill the tunnel")
+    ngrok.disconnect(http_tunnel.public_url)
+    ngrok.kill()
