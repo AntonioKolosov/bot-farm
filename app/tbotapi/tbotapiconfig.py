4892f6d401a35da1df1d4033bab4837aede2fe1a
“AntonioKolosov” <anton.kkolosov@gmail.com>
2023-04-26 08:53:28 +0000

    Move low level methods to tbotutils file as functions

--- app/tbotapi/tbotapiconfig.py
+++ app/tbotapi/tbotapiconfig.py
@@ -3,10 +3,9 @@
 
 
 import os
-from typing import Dict, Union
+from typing import Dict
 
-import urllib.parse as parse
-from httpx import AsyncClient, Response
+from .tbotapiutils import make_url, request_get, request_post
 
 
 class TbotAPIConfig:
@@ -17,44 +16,25 @@ class TbotAPIConfig:
         token = os.environ.get("BOT_TOKEN")
         bot_url = os.environ.get("BOT_URL")
         api_url = os.environ.get("API_URL")
-        self.__token = token if token else ""
+        token = token if token else ""
         self.__bot_url = bot_url if bot_url else ""
+        self.__bot_url = self.__bot_url.format(key=token, method="")
         self.__api_url = f'{api_url}/hook' if api_url else ""
 
     async def set_webhook(self) -> bool:
         """"""
         params = {"url": self.__api_url}
-        url = self.__make_url("setWebhook", params)
-        resp = await self.__request_get(url)
+        url = make_url(self.__bot_url, "setWebhook", params)
+        resp = await request_get(url)
         return resp.status_code == 200
 
     async def unset_webhook(self) -> bool:
         """"""
-        url = self.__make_url("deleteWebhook")
-        resp = await self.__request_get(url)
+        url = make_url(self.__bot_url, "deleteWebhook")
+        resp = await request_get(url)
         return resp.status_code == 200
 
     async def send_a_message(self, message: Dict) -> bool:
-        url = self.__make_url('sendMessage')
-        resp = await self.__request_post(url, message)
+        url = make_url(self.__bot_url, 'sendMessage')
+        resp = await request_post(url, message)
         return resp.status_code == 200
-
-    def __make_url(self, method: str,
-                   params: Union[Dict, None] = None) -> str:
-        """Make url for connection with bot"""
-        url = self.__bot_url.format(key=self.__token, method=method)
-        if params:
-            url = f'{url}?{parse.urlencode(params)}'
-        return url
-
-    async def __request_get(self, url: str) -> Response:
-        ''''''
-        async with AsyncClient() as client:
-            resp = await client.get(url)
-            return resp
-
-    async def __request_post(self, url: str, payload: dict) -> Response:
-        ''''''
-        async with AsyncClient() as client:
-            resp = await client.post(url, json=payload)
-            return resp
