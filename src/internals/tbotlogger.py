fc9e4f2725e7bbab0cbacf194878f68230304eb6
“AntonioKolosov” <anton.kkolosov@gmail.com>
2023-12-03 09:46:12 +0000

    Logs to terminal

--- src/internals/tbotlogger.py
+++ src/internals/tbotlogger.py
@@ -20,12 +20,12 @@ class TBotLogger:
     def __init__(self) -> None:
         """"""
         log_level = cfg.log_level
-        log_path = cfg.log_path
+        log_path = cfg.log_path  # ignoreqa F841
         log_level = log_levels.get(log_level, logging.WARNING)
         logging.basicConfig(level=log_level,
                             format="%(asctime)s %(levelname)s %(message)s",
                             datefmt="%Y-%m-%d %H:%M:%S",
-                            filename=log_path)
+                            handlers=[logging.StreamHandler(sys.stdout)])
 
     def log_info(self, message: str) -> None:
         file = sys._getframe(1).f_code.co_filename
