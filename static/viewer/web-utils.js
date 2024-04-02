d400449ee6a94e142fd48e3af25073366622f9ce
“AntonioKolosov” <anton.kkolosov@gmail.com>
2024-04-02 09:22:19 +0000

    static folder fix

--- static/viewer/web-utils.js
+++ static/viewer/web-utils.js
@@ -1,7 +1,7 @@
 // @ts-nocheck
 // Be interactions
 // const BE_LOCATOR = 'messintegrator.onrender.com';
-const BE_LOCATOR = 'messintegrator.onrender.com';
+const BE_LOCATOR = '7dc5-129-159-137-238.ngrok-free.app';
 
 const url = `wss:${BE_LOCATOR}/ws`
 const ws = new WebSocket(url);
